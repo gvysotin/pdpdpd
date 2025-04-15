ff07f62e3d3c3bdf2e79f5586708ddd5dcd1f78b
gvysotin <spideful@yandex.ru>
2025-04-15 12:55:08 +0800

    Major changes

---
+++ app/Http/Controllers/FollowerController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class FollowerController extends Controller
+{
+
+    public function follow(User $user)
+    {
+        // dd($user);
+        $follower = Auth::user(); // опредляем аутентифицированного пользователя
+        $follower->followings()->attach($user);
+        // первый метод даёт понять что выбрано отношение followings.
+        // второй метод создаёт запись, в него мы передаём id на кого подписываемся или сам объект user.
+        return redirect()->route("users.show", $user->id)->with("success","Followed successfully!");
+    }
+
+    public function unfollow(User $user)
+    {
+        // dd($user);
+        $follower = Auth::user(); // опредляем аутентифицированного пользователя
+        $follower->followings()->detach($user);
+        // первый метод даёт понять что выбрано отношение followings.
+        // второй метод создаёт запись, в него мы передаём id на кого подписываемся или сам объект user.
+        return redirect()->route("users.show", $user->id)->with("success","UnFollowed successfully!");
+
+    }
+
+
+}
---
+++ app/Http/Controllers/admin/TestMiddleware1.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+
+class TestMiddleware1 extends Controller
+{
+
+    /**
+     * Handle the incoming request.
+     */
+    public function __invoke(Request $request)
+    {
+
+        Log::info('inside TestMiddleware1 Controller');
+        //dd('Break');
+
+
+
+        // //dd('Сработал метод __invoke');
+        // $user = Auth::user();
+
+        // dump('Если мы дошли до сюда, пользователь админ, ниже объект $request');
+        // dump($request);
+
+
+        // $data = $request->all();
+        // dump($data);
+
+        // $user = Auth::user();
+
+        // $data = $request->all();
+        // $ip = $request->ip();
+
+        // if ($request->isMethod('post')) {
+        //     dump('Это POST-запрос');
+        // } else if ($request->isMethod('get')) {
+        //     dump('Это GET-запрос');
+        // }
+
+        // $method = $request->method();
+        // $url = $request->url();
+        // $fullUrl = $request->fullUrl();
+        // $path = $request->path();
+        // $server = $request->server();
+        // $cookie = $request->cookie();
+
+
+        // dump($cookie);
+        // dump($server);
+        // dump($method);
+        // dump($url);
+        // dump($fullUrl);
+        // dump($path);
+
+        // dump($user);
+        // dump($data);
+        // dump($ip);
+        // dd($request);
+    }
+}
