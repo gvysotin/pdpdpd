1becf51bddac8f90eb2cdfebd62cd4994d77acf5
gvysotin <spideful@yandex.ru>
2025-04-23 04:01:15 +0800

    Added VO

---
+++ app/ValueObjects/HashedPassword.php
@@ -0,0 +1,16 @@
+<?php
+
+final class HashedPassword
+{
+    public readonly string $value;
+
+    public function __construct(string $value)
+    {
+        $this->value = $value;
+    }
+
+    public function __toString(): string
+    {
+        return $this->value;
+    }
+}
---
+++ app/ValueObjects/PlainPassword.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\ValueObjects;
+
+use HashedPassword;
+use Illuminate\Support\Facades\Hash;
+use InvalidArgumentException;
+
+final class PlainPassword
+{
+    public readonly string $value;
+
+    public function __construct(string $value)
+    {
+        if (strlen($value) < 8) {
+            throw new InvalidArgumentException("Password must be at least 8 characters.");
+        }
+
+        $this->value = $value;
+    }
+
+    public function hash(): HashedPassword
+    {
+        return new HashedPassword(Hash::make($this->value));
+    }
+}
