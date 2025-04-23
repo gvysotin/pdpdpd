4218153365d5df76fea4fb05e9ae03d22c2ff351
gvysotin <spideful@yandex.ru>
2025-04-23 09:37:16 +0800

    Minor changes

---
+++ app/Jobs/SendWelcomeEmailJob.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Models\User;
+use App\Services\Interfaces\EmailNotificationServiceInterface;
+use Illuminate\Queue\{InteractsWithQueue, SerializesModels};
+use Illuminate\Support\Facades\Log;
+use Throwable;
+
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Queue\Queueable;
+
+class SendWelcomeEmailJob implements ShouldQueue
+{
+    use InteractsWithQueue, Queueable, SerializesModels;
+
+    public $tries = 3;
+    public $backoff = 10;
+    public $timeout = 30;
+
+    /**
+     * Create a new job instance.
+     */
+    public function __construct(
+        protected User $user
+    )
+    {}
+
+    /**
+     * Execute the job.
+     */
+    public function handle(EmailNotificationServiceInterface $emailService): void
+    {
+        if ($this->user->hasReceivedWelcomeEmail()) {
+            Log::info("Welcome email already sent to user ID: {$this->user->id}");
+            return;
+        }
+
+        Log::info("Sending welcome email to user ID: {$this->user->id}");
+
+        try {
+            $emailService->sendWelcomeEmail($this->user);
+            $this->user->markWelcomeEmailAsSent();
+
+            Log::info("Welcome email sent and timestamp updated for user ID: {$this->user->id}");
+        } catch (Throwable $e) {
+            Log::error('Error sending welcome email', [
+                'user_id' => $this->user->id,
+                'error' => $e->getMessage(),
+            ]);
+
+            throw $e; // Laravel сам повторит Job
+        }        
+
+    }
+}
