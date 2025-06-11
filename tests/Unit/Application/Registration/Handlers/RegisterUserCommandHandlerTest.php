71231946394d8b5464bbede6d1a91e885400f5e0
gvysotin <spideful@yandex.ru>
2025-06-11 10:16:56 +0800

    Added tests

--- tests/Unit/Application/Registration/Handlers/RegisterUserCommandHandlerTest.php
+++ tests/Unit/Application/Registration/Handlers/RegisterUserCommandHandlerTest.php
@@ -7,6 +7,8 @@ use App\Application\Registration\Handlers\RegisterUserCommandHandler;
 use App\Domain\Registration\Contracts\EmailSpecificationInterface;
 use App\Domain\Registration\Contracts\UserCreatorInterface;
 use App\Domain\Registration\DTO\UserRegistrationData;
+use App\Domain\Registration\Exceptions\DuplicateEmailException;
+use App\Domain\Registration\Exceptions\UserPersistenceException;
 use App\Domain\Registration\ValueObjects\Email;
 use App\Domain\Registration\ValueObjects\PlainPassword;
 use App\Domain\Shared\Contracts\TransactionManagerInterface;
@@ -18,6 +20,7 @@ use Illuminate\Support\Facades\Event;
 use Illuminate\Support\Facades\Log;
 use Mockery;
 use PHPUnit\Framework\Attributes\Test;
+use RuntimeException;
 use Tests\TestCase;
 
 class RegisterUserCommandHandlerTest extends TestCase
@@ -42,7 +45,7 @@ class RegisterUserCommandHandlerTest extends TestCase
 
         $createdUser = User::factory()->make();
 
-        // Email проверка
+        // Проверка email
         $uniqueEmailSpec
             ->shouldReceive('check')
             ->once()
@@ -58,8 +61,10 @@ class RegisterUserCommandHandlerTest extends TestCase
         $transaction->shouldReceive('begin')->once();
         $transaction->shouldReceive('commit')->once();
         $transaction->shouldReceive('rollback')->never();
+        // Проверяется, что транзакция начинается, завершается, и не откатывается, а afterCommit() запускает callback, 
+        // который вызывает событие UserRegistered.
         $transaction->shouldReceive('afterCommit')->once()->andReturnUsing(function (callable $callback) {
-            $callback(); // имитируем выполнение callback'а
+            $callback(); // имитируем post-commit callback
         });
 
         $handler = new RegisterUserCommandHandler(
@@ -73,9 +78,147 @@ class RegisterUserCommandHandlerTest extends TestCase
 
         $result = $handler->handle($command);
 
+        // Подтверждаем, что OperationResult — успешен.
         $this->assertTrue($result->succeeded());
 
+        // Laravel проверяет, что событие UserRegistered было отправлено с тем же экземпляром пользователя, который вернул UserCreator.
         Event::assertDispatched(UserRegistered::class, fn($event) => $event->user === $createdUser);
+
+        // Закрывает все моки и проверяет, что все ожидаемые вызовы произошли.
+        Mockery::close();
+    }
+
+    #[Test]
+    public function it_fails_registration_due_to_duplicate_email(): void
+    {
+        Event::fake(); // Имитация очереди событий
+
+        $userCreator = Mockery::mock(UserCreatorInterface::class);
+        $uniqueEmailSpec = Mockery::mock(EmailSpecificationInterface::class);
+        $logger = Log::spy();
+        $transaction = Mockery::mock(TransactionManagerInterface::class);
+
+        $dto = new UserRegistrationData(
+            name: 'Test User',
+            email: new Email('test@example.com'),
+            password: new PlainPassword('secret123')
+        );
+
+        // Указываем, что email не уникален
+        $uniqueEmailSpec
+            ->shouldReceive('check')
+            ->once()
+            ->andThrow(new DuplicateEmailException);
+
+        $transaction->shouldReceive('begin')->never();
+        $transaction->shouldReceive('commit')->never();
+        $transaction->shouldReceive('rollback')->once();
+
+        $handler = new RegisterUserCommandHandler(
+            userCreator: $userCreator,
+            uniqueEmailSpec: $uniqueEmailSpec,
+            logger: $logger,
+            transaction: $transaction
+        );
+
+        $command = new RegisterUserCommand($dto);
+        $result = $handler->handle($command);
+
+        $this->assertTrue($result->failed());
+        $this->assertSame('Email is already registered', $result->message());
+
+        Event::assertNotDispatched(UserRegistered::class);
+
+        Mockery::close();
+    }
+
+
+    #[Test]
+    public function it_rolls_back_if_user_save_fails(): void
+    {
+        Event::fake(); // Имитация очереди событий
+
+        $userCreator = Mockery::mock(UserCreatorInterface::class);
+        $uniqueEmailSpec = Mockery::mock(EmailSpecificationInterface::class);
+        $logger = Log::spy();
+        $transaction = Mockery::mock(TransactionManagerInterface::class);
+
+        $dto = new UserRegistrationData(
+            name: 'Test User',
+            email: new Email('test@example.com'),
+            password: new PlainPassword('secret123')
+        );
+
+        $uniqueEmailSpec->shouldReceive('check')->once();
+
+        $transaction->shouldReceive('begin')->once();
+        $transaction->shouldReceive('rollback')->once();
+        $transaction->shouldReceive('afterCommit')->never();
+        $transaction->shouldReceive('commit')->never();
+
+        $userCreator->shouldReceive('create')
+            ->once()
+            ->andThrow(new UserPersistenceException('DB error'));
+
+        $handler = new RegisterUserCommandHandler(
+            userCreator: $userCreator,
+            uniqueEmailSpec: $uniqueEmailSpec,
+            logger: $logger,
+            transaction: $transaction
+        );
+
+        $command = new RegisterUserCommand($dto);
+        $result = $handler->handle($command);
+
+        $this->assertTrue($result->failed());
+        $this->assertSame('DB error', $result->message());
+
+        Event::assertNotDispatched(UserRegistered::class);
+
+        Mockery::close();
+    }
+
+    #[Test]
+    public function it_rolls_back_on_unexpected_error(): void
+    {
+        Event::fake(); // Имитация очереди событий
+
+        $userCreator = Mockery::mock(UserCreatorInterface::class);
+        $uniqueEmailSpec = Mockery::mock(EmailSpecificationInterface::class);
+        $logger = Log::spy();
+        $transaction = Mockery::mock(TransactionManagerInterface::class);
+
+        $dto = new UserRegistrationData(
+            name: 'Test User',
+            email: new Email('test@example.com'),
+            password: new PlainPassword('secret123')
+        );
+
+        $uniqueEmailSpec->shouldReceive('check')->once();
+
+        $transaction->shouldReceive('begin')->once();
+        $transaction->shouldReceive('rollback')->once();
+        $transaction->shouldReceive('commit')->never();
+        $transaction->shouldReceive('afterCommit')->never();
+
+        $userCreator->shouldReceive('create')
+            ->once()
+            ->andThrow(new RuntimeException('Unexpected failure'));
+
+        $handler = new RegisterUserCommandHandler(
+            userCreator: $userCreator,
+            uniqueEmailSpec: $uniqueEmailSpec,
+            logger: $logger,
+            transaction: $transaction
+        );
+
+        $command = new RegisterUserCommand($dto);
+        $result = $handler->handle($command);
+
+        $this->assertTrue($result->failed());
+        $this->assertSame('Failed to register user', $result->message());
+
+        Event::assertNotDispatched(UserRegistered::class);
 
         Mockery::close();
     }
